35508f0b808f36a189fdabd3ed6a07c9a7bb7f1e
Gimantha Bandara <gimantha@wso2.com>
2015-01-27 12:40:48 +0530

    Changed the test cases since the API changed.

--- modules/integration/tests-integration/src/test/java/org/wso2/bam/integration/tests/restservice/analytics/AnalyticsRestTestCase.java
+++ modules/integration/tests-integration/src/test/java/org/wso2/bam/integration/tests/restservice/analytics/AnalyticsRestTestCase.java
@@ -1,5 +1,3 @@
-package org.wso2.bam.integration.tests.restservice.analytics;
-
 /*
 * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 *
@@ -18,6 +16,8 @@ package org.wso2.bam.integration.tests.restservice.analytics;
 * under the License.
 */
 
+package org.wso2.bam.integration.tests.restservice.analytics;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.testng.Assert;
@@ -29,17 +29,23 @@ import org.wso2.carbon.automation.test.utils.http.client.HttpRequestUtil;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.wso2.bam.analytics.rest.beans.*;
+
+import com.google.gson.Gson;
 public class AnalyticsRestTestCase extends BAMIntegrationTest {
 
     private static final Log log = LogFactory.getLog(AnalyticsRestTestCase.class);
     private static final String TABLE_NAME = "testtable";
     private static final long ONE_HOUR_MILLISECOND = 3600000;
-
+    private static final Gson gson = new Gson();
+    
     private Map<String, String> headers;
-
 
     @BeforeClass(alwaysRun = true)
     protected void init() throws Exception {
@@ -55,65 +61,95 @@ public class AnalyticsRestTestCase extends BAMIntegrationTest {
         URL restUrl = new URL(TestConstants.ANALYTICS_ENDPOINT_URL + TABLE_NAME);
 
         HttpResponse response = HttpRequestUtil.doPost(restUrl, "", headers);
-
+        log.info("Response: " + response.getData());
         Assert.assertEquals(response.getResponseCode(), 200, "Status code is different");
         Assert.assertTrue(response.getData().
                 contains("Successfully created table: " + TABLE_NAME + " for tenantId: -1234"));
-
-        log.info("Response: " + response.getData());
     }
 
-    @Test(groups = "wso2.bam", description = "Create records", dependsOnMethods = "createTable")
-    public void createRecords() throws Exception {
+    @Test(groups = "wso2.bam", description = "Create records with optional paramters", dependsOnMethods = "createTable")
+	public void createRecordsWithoutOptionalParams() throws Exception {
+
+		log.info("Executing create records test case ...");
+		URL restUrl = new URL(TestConstants.ANALYTICS_RECORD_ENDPOINT_URL);
+
+		List<RecordBean> recordList = new ArrayList<RecordBean>();
+		Map<String, Object> values = new HashMap<String, Object>();
+
+		RecordBean record1 = new RecordBean();
+		record1.setTableName(TABLE_NAME);
+		values.put("key1", "value1");
+		values.put("key2", "value2");
+		values.put("key3", "value3");
+		values.put("key4", "value4");
+		values.put("key5", "value5");
+		record1.setValues(values);
+
+		values = new HashMap<String, Object>();
+		RecordBean record2 = new RecordBean();
+		record2.setTableName(TABLE_NAME);
+		values.put("key7", "value1");
+		values.put("key6", "value2");
+		values.put("key9", "value3");
+		values.put("key0", "value4");
+		values.put("key4", "value5");
+		record2.setValues(values);
+
+		recordList.add(record1);
+		recordList.add(record2);
+
+		HttpResponse response = HttpRequestUtil.doPost(restUrl, gson.toJson(recordList), headers);
+		log.info("Response: " + response.getData());
+		Assert.assertEquals(response.getResponseCode(), 200, "Status code is different");
+		Assert.assertTrue(response.getData().contains("Successfully added records"));
+	}
+    
+    @Test(groups = "wso2.bam", description = "Create records without optional params", dependsOnMethods = "createTable")
+    public void createRecordsWithOptionalParams() throws Exception {
 
         log.info("Executing create records test case ...");
-        URL restUrl = new URL(TestConstants.ANALYTICS_RECORD_ENDPOINT_URL);
         long currentTime = System.currentTimeMillis();
-
-        String payload = "[\n" +
-                         "    {\n" +
-                         "        \"id\": \"sample1\",\n" +
-                         "        \"tableName\": \"" + TABLE_NAME + "\",\n" +
-                         "        \"timestamp\": " + currentTime + ",\n" +
-                         "        \"values\": \n" +
-                         "        {\n" +
-                         "            \"timestamp\": \"2015-01-22 12:30:49,264\",\n" +
-                         "            \"message\"  : \"File input event adaptor loading listeners \",\n" +
-                         "            \"product\"  : \"wso2am\",\n" +
-                         "            \"level\"    : \"INFO\",\n" +
-                         "            \"component\": \"org.wso2.carbon.event.input.adaptor.file.FileEventAdaptorType\"\n" +
-                         "        }\n" +
-                         "\n" +
-                         "    },\n" +
-                         "\n" +
-                         "{\n" +
-                         "        \"id\": \"sample2\",\n" +
-                         "        \"tableName\": \"" + TABLE_NAME + "\",\n" +
-                         "        \"timestamp\": " + currentTime + ",\n" +
-                         "        \"values\": \n" +
-                         "        {\n" +
-                         "            \"timestamp\": \"2015-01-22 12:30:49,264\",\n" +
-                         "            \"message\"  : \"File input event adaptor loading listeners \",\n" +
-                         "            \"product\"  : \"wso2am\",\n" +
-                         "            \"level\"    : \"INFO\",\n" +
-                         "            \"component\": \"org.wso2.carbon.event.input.adaptor.file.FileEventAdaptorType\"\n" +
-                         "        }\n" +
-                         "\n" +
-                         "    }\n" +
-                         "]";
-
-        HttpResponse response = HttpRequestUtil.doPost(restUrl, payload.trim(), headers);
-
+        URL restUrl = new URL(TestConstants.ANALYTICS_RECORD_ENDPOINT_URL);
+        
+        List<RecordBean> recordList = new ArrayList<RecordBean>();
+        Map<String, Object> values = new LinkedHashMap<String, Object>();
+        
+        RecordBean  record1 = new RecordBean();
+        record1.setId("id1");
+        record1.setTableName(TABLE_NAME);
+        record1.setTimestamp(currentTime);
+        values.put("key1", "value1");
+        values.put("key2", "value2");
+        values.put("key3", "value3");
+        values.put("key4", "value4");
+        values.put("key5", "value5");
+        record1.setValues(values);
+        
+        values = new LinkedHashMap<String, Object>();
+        RecordBean  record2 = new RecordBean();
+        record2.setId("id2");
+        record2.setTableName(TABLE_NAME);
+        record2.setTimestamp(currentTime);
+        values.put("key7", "value1");
+        values.put("key6", "value2");
+        values.put("key9", "value3");
+        values.put("key0", "value4");
+        values.put("key4", "value5");
+        record2.setValues(values);
+        
+        recordList.add(record1);
+        recordList.add(record2);
+        
+        HttpResponse response = HttpRequestUtil.doPost(restUrl, gson.toJson(recordList), headers);
+        log.info("Response: " + response.getData());
         Assert.assertEquals(response.getResponseCode(), 200, "Status code is different");
         Assert.assertTrue(response.getData().contains("Successfully added records"));
-
-        log.info("Response: " + response.getData());
     }
 
-    @Test(groups = "wso2.bam", description = "Get records", dependsOnMethods = "createRecords")
+    @Test(groups = "wso2.bam", description = "Get records", dependsOnMethods = "createRecordsWithOptionalParams")
     public void getRecordsWithoutPagination() throws Exception {
 
-        log.info("Executing create records test case ...");
+        log.info("Executing get records test case ...");
         long currentTime = System.currentTimeMillis();
         StringBuilder restUrl = new StringBuilder();
         restUrl.append(TestConstants.ANALYTICS_RECORD_ENDPOINT_URL);
@@ -122,12 +158,11 @@ public class AnalyticsRestTestCase extends BAMIntegrationTest {
         restUrl.append(currentTime - ONE_HOUR_MILLISECOND);
         restUrl.append("/");
         restUrl.append(currentTime + ONE_HOUR_MILLISECOND);
-
         HttpResponse response = HttpRequestUtil.doGet(restUrl.toString(), headers);
-
-        Assert.assertEquals(response.getResponseCode(), 200, "Status code is different");
-        Assert.assertTrue(response.getData().contains("\"values\":{\"product\":\"wso2am\",\"message\":\"File input event adaptor loading listeners \",\"timestamp\":\"2015-01-22 12:30:49,264\",\"level\":\"INFO\",\"component\":\"org.wso2.carbon.event.input.adaptor.file.FileEventAdaptorType\"}"));
-
+        
         log.info("Response: " + response.getData());
+        Assert.assertEquals(response.getResponseCode(), 200, "Status code is different");
+        Assert.assertTrue(response.getData().contains("\"values\":{\"key1\":\"value1\",\"key2\":\"value2\"," + 
+        			"\"key3\":\"value3\",\"key4\":\"value4\",\"key5\":\"value5\"}"));
     }
 }
