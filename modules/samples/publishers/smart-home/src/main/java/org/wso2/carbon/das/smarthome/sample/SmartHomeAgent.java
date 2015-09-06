76bab00d979c4ea978a8f9c23de8e6dec9049e54
gokul <royalgok@gmail.com>
2015-09-06 13:04:19 +0530

    Updating Smart Home sample with different data and format

--- modules/samples/publishers/smart-home/src/main/java/org/wso2/carbon/das/smarthome/sample/SmartHomeAgent.java
+++ modules/samples/publishers/smart-home/src/main/java/org/wso2/carbon/das/smarthome/sample/SmartHomeAgent.java
@@ -34,10 +34,17 @@ import java.util.UUID;
 
 
 public class SmartHomeAgent {
-    private static final String SMART_HOME_STREAM = "org.wso2.sample.smart.home.data";
+    private static final String SMART_HOME_STREAM = "org.wso2.das.sample.smart.home.data";
     private static final String VERSION = "1.0.0";
     private static final int defaultThriftPort = 7611;
     private static final int defaultBinaryPort = 9611;
+    private static final Random RAND = new Random();
+    private static int count;
+
+    private static final String[] CITIES = {"New York", "Los Angeles", "Chicago", "Dallas",
+            "Miami", "Salt Lake City", "Seattle", "Phoenix", "St. Louis", "Indianapolis"};
+
+    private static final String[] STATES = {"NY", "CA", "IL", "TX", "FL", "UT", "WA", "AZ", "MO", "IN"};
 
     public static void main(String[] args) throws DataEndpointAuthenticationException,
             DataEndpointAgentConfigurationException,
@@ -68,10 +75,16 @@ public class SmartHomeAgent {
         String username = getProperty("username", "admin");
         String password = getProperty("password", "admin");
 
+        if (args[0] == null || args[0].isEmpty() || args[0].equals("count")) {
+            count = 3000;
+        } else {
+            count = Integer.parseInt(args[0]);
+        }
+
         DataPublisher dataPublisher = new DataPublisher(type, url, authURL, username, password);
 
         String streamId = DataBridgeCommonsUtils.generateStreamId(SMART_HOME_STREAM, VERSION);
-        publishLogEvents(dataPublisher, streamId);
+        publishEvents(dataPublisher, streamId);
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -94,26 +107,20 @@ public class SmartHomeAgent {
         return filePath.getAbsolutePath() + File.separator + "data-agent-conf.xml";
     }
 
-    private static void publishLogEvents(DataPublisher dataPublisher, String streamId) throws FileNotFoundException, SocketException, UnknownHostException {
-        String host;
-        Random rand = new Random();
+    private static void publishEvents(DataPublisher dataPublisher, String streamId) throws FileNotFoundException, SocketException, UnknownHostException {
         int ctr = 0;
 
-        if (getLocalAddress() != null) {
-            host = getLocalAddress().getHostAddress();
-        } else {
-            host = "localhost"; // Defaults to localhost
-        }
-        for (int i = 0; i < 3000; i++) {
+        for (int i = 0; i < count; i++) {
+            int idx = RAND.nextInt(10);
             Object[] payload = new Object[]{
-                    UUID.randomUUID().toString(),
-                    rand.nextFloat() * 500,
-                    rand.nextBoolean(),
-                    rand.nextInt(7),
-                    rand.nextInt(21),
-                    rand.nextInt(6)
+                    RAND.nextInt(21) + 1,
+                    CITIES[idx],
+                    STATES[idx],
+                    RAND.nextInt(7) + 1,
+                    RAND.nextFloat() * 500,
+                    RAND.nextBoolean()
             };
-            Event event = new Event(streamId, System.currentTimeMillis(), new Object[]{host}, null, payload);
+            Event event = new Event(streamId, System.currentTimeMillis(), null, null, payload);
             dataPublisher.publish(event);
             ctr++;
         }
@@ -122,9 +129,9 @@ public class SmartHomeAgent {
     }
 
     public static InetAddress getLocalAddress() throws SocketException, UnknownHostException {
-        Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
-        while (ifaces.hasMoreElements()) {
-            NetworkInterface iface = ifaces.nextElement();
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface iface = interfaces.nextElement();
             Enumeration<InetAddress> addresses = iface.getInetAddresses();
 
             while (addresses.hasMoreElements()) {
