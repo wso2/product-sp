2eb3f8e50ff72ea20a96db8092f218ac81948312
Dunith Dhanushka <dunithd@gmail.com>
2015-09-07 09:21:20 -0500

    Fixing the map chart type in the Smart home sample

--- modules/samples/capps/Smart_Home/GadgetPeakUsageSEA_1.0.0/Peak_Usage_Range_-_Seattle/js/gadgetconf.js
+++ modules/samples/capps/Smart_Home/GadgetPeakUsageSEA_1.0.0/Peak_Usage_Range_-_Seattle/js/gadgetconf.js
@@ -1,1 +1,20 @@
-var gadgetConfig ={"id":"Peak_Usage_Range_-_Seattle","title":"Peak Usage Range - Seattle","datasource":"PEAK_DEVICE_USAGE_RANGE","type":"batch","columns":[{"name":"house_id","type":"INTEGER"},{"name":"usage_range","type":"FLOAT"}],"maxUpdateValue":0,"chartConfig":{"yAxis":1,"xAxis":0,"chartType":"area"},"domain":"carbon.super"};
+var gadgetConfig = {
+    "id": "Peak_Usage_Range_-_Seattle",
+    "title": "Peak Usage Range - Seattle",
+    "datasource": "PEAK_DEVICE_USAGE_RANGE",
+    "type": "batch",
+    "columns": [ {
+        "name": "usage_range",
+        "type": "FLOAT"
+    },{
+        "name": "house_id",
+        "type": "INTEGER"
+    }],
+    "maxUpdateValue": 0,
+    "chartConfig": {
+        "yAxis": 0,
+        "xAxis": 1,
+        "chartType": "area"
+    },
+    "domain": "carbon.super"
+};
