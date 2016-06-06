8bf072ab7efa3d3a5d019f033268c89742752347
dunithd <dunithd@gmail.com>
2016-06-06 16:48:22 +0530

    Fixing Smart home sample

--- modules/samples/capps/Smart_Home/GadgetUsageCities_1.0.0/Usage_-_Cities/js/gadgetconf.js
+++ modules/samples/capps/Smart_Home/GadgetUsageCities_1.0.0/Usage_-_Cities/js/gadgetconf.js
@@ -1,27 +1,21 @@
-var gadgetConfig = {
-    "id": "usage-cities",
-    "title": "usage-cities",
-    "datasource": "CITY_USAGE",
-    "type": "batch",
-    "columns": [{
-        "name": "max_usage",
-        "type": "FLOAT"
-    }, {
-        "name": "metro_area",
-        "type": "STRING"
-    }, {
-        "name": "avg_usage",
-        "type": "FLOAT"
-    }, {
-        "name": "min_usage",
-        "type": "FLOAT"
-    }],
-    "maxUpdateValue": 0,
-    "chartConfig": {
-        "chartType": "line",
-        "yAxis": [0, 2, 3],
-        "xAxis": 1,
-        "interpolationMode": "monotone"
-    },
-    "domain": "carbon.super"
+var config = {
+
+	providerConfig : {
+		tableName : "SPEED",
+		schema: [
+			"metro_area" : "ordinal",
+			"min_usage" : "linear",
+		],
+		timeFrom : 123456,
+		timeTo : 123456,
+		filter: "foo"
+	},
+	chartConfig: {
+		x: "metro_area",
+        charts: [{ type: "bar", y: "avg_usage" }],
+        padding: { "top": 20, "left": 50, "bottom": 20, "right": 80 },
+        range: false,
+        height: 300
+	}
+
 };
