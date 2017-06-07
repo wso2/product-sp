3972f7bb78cd56afa4066cd9a9c336a67b22d42a
Yasara Dissanayake <yasaradiss101@gmail.com>
2017-06-08 02:21:05 +0530

    adding http sample

---
+++ modules/samples/artifacts/0010/README.txt
@@ -0,0 +1,5 @@
+[1] Copy {WSO2DASHome}/samples/0010/http-io-sample.siddhi file to {WSO2DASHome}/deployment/siddhi-files.
+[2] Navigate to {WSO2DASHome}/bin and start the server using ./worker.sh
+[3] Navigate to {WSO2DASHome}/samples/sample-clients/http-server and run the "ant" command without arguments to start the HTTP Server
+[4] Navigate to {WSO2DASHome}/samples/sample-clients/http-client and run the "ant" command without arguments to publish events to HTTP/HTTPS endpoint of the DAS server.
+
---
+++ modules/samples/sample-clients/http-server/src/main/java/org/wso2/das/http/server/HttpServerListener.java
@@ -0,0 +1,74 @@
+/*
+ *  Copyright (c) 2017 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2.das.http.server;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Test Server Listener Manger.
+ */
+public class HttpServerListener implements HttpHandler {
+    private static final Logger logger = Logger.getLogger(HttpServerListener.class);
+    private AtomicBoolean isEventArraved = new AtomicBoolean(false);
+    private StringBuilder strBld;
+    private Headers headers;
+
+    HttpServerListener() {
+    }
+
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+        // Get the paramString form the request
+        String line;
+        headers = t.getRequestHeaders();
+        InputStream is = t.getRequestBody();
+        BufferedReader in = new BufferedReader(new InputStreamReader(is)); // initiating
+        strBld = new StringBuilder();
+        while ((line = in.readLine()) != null) {
+            strBld = strBld.append(line).append("\n");
+            System.out.print(line + "\n");
+        }
+
+        logger.info("Event Arrived: " + strBld.toString());
+        isEventArraved.set(true);
+    }
+
+    String getData() {
+        isEventArraved = new AtomicBoolean(false);
+        return strBld.toString();
+    }
+
+    Headers getHeaders() {
+        return headers;
+    }
+
+    boolean iaMessageArrive() {
+        return isEventArraved.get();
+    }
+
+}
