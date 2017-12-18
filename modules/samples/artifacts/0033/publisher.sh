256cc33a40a31f8b409418b4d347a2ab5e98e98e
tharindulak <tharindulak>
2017-12-18 09:54:08 +0530

    Changes to sample logs

--- modules/samples/artifacts/0033/publisher.sh
+++ modules/samples/artifacts/0033/publisher.sh
@@ -2,6 +2,7 @@
 for i in {1..50}; do
   curl -X POST \
         http://localhost:9090/simulation/single \
+        -u admin:admin \
         -H 'content-type: text/plain' \
         -d '{
         "siddhiAppName": "timeSeriesExtensionSample",
