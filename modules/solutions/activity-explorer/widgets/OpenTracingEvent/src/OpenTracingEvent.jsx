b4c6878fbee0fbc3e83d0510b5d2020c16e97ff5
lasanthaS <lasn1987@gmail.com>
2018-06-08 00:29:40 +0530

    Fix styles in activity explorer dashboard widgets

--- modules/solutions/activity-explorer/widgets/OpenTracingEvent/src/OpenTracingEvent.jsx
+++ modules/solutions/activity-explorer/widgets/OpenTracingEvent/src/OpenTracingEvent.jsx
@@ -1,9 +1,29 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
 import React, {Component} from "react";
 import Widget from "@wso2-dashboards/widget";
 import VizG from 'react-vizgrammar';
 import {Scrollbars} from 'react-custom-scrollbars';
 import Axios from 'axios';
 import _ from 'lodash';
+import './OpenTracingEvent.css';
 
 const COOKIE = 'DASHBOARD_USER';
 
@@ -133,10 +153,10 @@ class OpenTracingEvent extends Widget {
                 <Scrollbars style={{height: this.state.height}}>
                     <div
                         style={{
-                            marginTop: "5px",
                             width: this.props.glContainer.width,
                             height: this.props.glContainer.height,
                         }}
+                        className="event-table-wrapper"
                     >
                         <VizG
                             config={this.tableConfig}
@@ -145,6 +165,7 @@ class OpenTracingEvent extends Widget {
                             append={false}
                             height={this.props.glContainer.height}
                             width={this.props.glContainer.width}
+                            theme={this.props.muiTheme.name}
                         />
                     </div>
                 </Scrollbars>
