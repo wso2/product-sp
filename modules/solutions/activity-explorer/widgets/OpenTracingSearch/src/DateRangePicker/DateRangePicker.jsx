4e95375b5e8c92fa76b558f692fb70c9170a2093
lasanthaS <lasn1987@gmail.com>
2018-06-13 11:21:11 +0530

    Add date picker to OpenTracingSearch

---
+++ modules/solutions/activity-explorer/widgets/OpenTracingSearch/src/DateRangePicker/DateRangePicker.jsx
@@ -0,0 +1,138 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React, { Component } from 'react';
+import GranularityModeSelector from './GranularityModeSelector';
+import CustomTimeRangeSelector from './CustomTimeRangeSelector';
+import Moment from 'moment';
+
+export default class DateRangePicker extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            granularityMode: null
+        };
+        this.onGranularityChanged = this.onGranularityChanged.bind(this);
+        this.onTimeChanged = this.onTimeChanged.bind(this);
+    }
+
+    onGranularityChanged(granularityMode) {
+        this.setState({ granularityMode });
+        if (granularityMode === 'custom') {
+            return;
+        }
+        let from = '';
+        switch (granularityMode) {
+            case '1 Min':
+                from = Moment().subtract(1, 'minutes').toDate();
+                break;
+            case '15 Min':
+                from = Moment().subtract(15, 'minutes').toDate();
+                break;
+            case '1 Hour':
+                from = Moment().subtract(1, 'hours').toDate();
+                break;
+            case '1 Day':
+                from = Moment().subtract(1, 'days').toDate();
+                break;
+            case '7 Days':
+                from = Moment().subtract(7, 'days').toDate();
+                break;
+            case '1 Month':
+                from = Moment().subtract(1, 'months').toDate();
+                break;
+            case '3 Months':
+                from = Moment().subtract(3, 'months').toDate();
+                break;
+            case '6 Months':
+                from = Moment().subtract(6, 'months').toDate();
+                break;
+            case '1 Year':
+                from = Moment().subtract(1, 'years').toDate();
+                break;
+        }
+
+        this.onTimeChanged(from, new Date());
+    }
+
+    onTimeChanged(from, to) {
+        if (this.props.onChange) {
+            this.props.onChange(from, to);
+        }
+    }
+
+    renderPredefinedTimeRangeSelector(mode) {
+        let startTime = null, endTime = null; //, granularity = null;
+
+        switch (mode) {
+            case '1 Min':
+                startTime = Moment().subtract(1, 'minutes').format("DD-MMM-YYYY hh:mm A");
+                endTime = Moment().format("DD-MMM-YYYY hh:mm A");
+                break;
+            case '15 Min':
+                startTime = Moment().subtract(15, 'minutes').format("DD-MMM-YYYY hh:mm A");
+                endTime = Moment().format("DD-MMM-YYYY hh:mm A");
+                break;
+            case '1 Hour':
+                startTime = Moment().subtract(1, 'hours').format("DD-MMM-YYYY hh:mm A");
+                endTime = Moment().format("DD-MMM-YYYY hh:mm A");
+                break;
+            case '1 Day':
+                startTime = Moment().subtract(1, 'days').format("DD-MMM-YYYY");
+                endTime = Moment().format("DD-MMM-YYYY");
+                break;
+            case '7 Days':
+                startTime = Moment().subtract(7, 'days').format("DD-MMM-YYYY");
+                endTime = Moment().format("DD-MMM-YYYY");
+                break;
+            case '1 Month':
+                startTime = Moment().subtract(1, 'months').format("MMM-YYYY");
+                endTime = Moment().format('MMM-YYYY');
+                break;
+            case '3 Months':
+                startTime = Moment().subtract(3, 'months').format('MMM-YYYY');
+                endTime = Moment().format('MMM-YYYY');
+                break;
+            case '6 Months':
+                startTime = Moment().subtract(6, 'months').format('MMM-YYYY');
+                endTime = Moment().format('MMM-YYYY');
+                break;
+            case '1 Year':
+                startTime = Moment().subtract(1, 'years').format('YYYY');
+                endTime = Moment().format('YYYY');
+                break;
+        }
+
+        return <div>{startTime} to {endTime}</div>;
+    }
+
+    render() {
+        return (
+            <div>
+                <GranularityModeSelector onChange={this.onGranularityChanged} />
+                {
+                    this.state.granularityMode === 'custom' ?
+                        <CustomTimeRangeSelector onChange={this.onTimeChanged} /> :
+                        this.renderPredefinedTimeRangeSelector(this.state.granularityMode)
+                }
+            </div>
+        );
+    }
+}
---
+++ modules/solutions/activity-explorer/widgets/OpenTracingSearch/src/DateRangePicker/GranularityModeSelector.jsx
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React, { Component } from 'react';
+import { FlatButton, IconButton } from 'material-ui';
+import HighGranularityMode from '@material-ui/icons/KeyboardArrowRight';
+import LowGranularityMode from '@material-ui/icons/KeyboardArrowLeft';
+
+export default class GranularityModeSelector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            granularityMode: 'high',
+            granularityModeValue: 'none',
+        };
+
+        this.granularityOptions = {
+            low: ['1 Min', '15 Mins', '1 Hour', '1 Day'],
+            high: ['1 Day', '7 Days', '1 Month', '3 Months', '6 Months', '1 Year']
+        };
+
+        this.onGranularityModeChange = this.onGranularityModeChange.bind(this);
+    }
+
+    onGranularityModeChange(value) {
+        this.setState({ granularityModeValue: value });
+        return this.props.onChange && this.props.onChange(value);
+    }
+
+    render() {
+        let { granularityMode } = this.state;
+        return (
+            <div>
+                <div>
+                    {
+                        this.granularityOptions[granularityMode].map(o => <FlatButton
+                            label={o}
+                            onClick={() => this.onGranularityModeChange(o)} />
+                        )
+                    }
+                    <IconButton onClick={() => this.setState({
+                        granularityMode: granularityMode === 'low' ? 'high' : 'low'
+                    })}>
+                        {
+                            granularityMode === 'low' ? <HighGranularityMode /> : <LowGranularityMode />
+                        }
+                    </IconButton>
+                    <FlatButton onClick={() => this.onGranularityModeChange('custom')}>Custom</FlatButton>
+                </div>
+            </div>
+        );
+    }
+}
