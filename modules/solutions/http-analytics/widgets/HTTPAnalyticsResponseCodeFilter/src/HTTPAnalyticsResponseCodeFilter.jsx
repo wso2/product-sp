19a136ff3a777fe747bfd586089177d034632a3a
Niveathika <niveathika@wso2.com>
2018-06-14 17:32:23 +0530

    Updated filter widgets for theme switching

--- modules/solutions/http-analytics/widgets/HTTPAnalyticsResponseCodeFilter/src/HTTPAnalyticsResponseCodeFilter.jsx
+++ modules/solutions/http-analytics/widgets/HTTPAnalyticsResponseCodeFilter/src/HTTPAnalyticsResponseCodeFilter.jsx
@@ -35,8 +35,74 @@ import Select from 'react-select';
 import {Scrollbars} from 'react-custom-scrollbars';
 import JssProvider from 'react-jss/lib/JssProvider';
 
-const customTheme = createMuiTheme({});
-const customStyles = {};
+const darkTheme = createMuiTheme({
+    palette: {
+        type: "dark"
+    }
+});
+
+const lightTheme = createMuiTheme({
+    palette: {
+        type: "light"
+    }
+});
+
+const customStyles = {
+    input: () => ({
+        color: 'white'
+    }),
+    multiValue: () => ({
+        borderRadius: 15,
+        display: 'flex',
+        flexWrap: 'wrap',
+        color: 'black',
+        fontSize: '90%',
+        overflow: 'hidden',
+        paddingLeft: 6,
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        backgroundColor: 'darkgrey',
+        minWidth: '20'
+    }),
+    singleValue: () => ({
+        display: 'flex',
+        flexWrap: 'wrap',
+        color: 'white',
+        fontSize: '95%',
+    }),
+    control: () => ({
+        height: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        minHeight: 30,
+        backgroundColor: 'rgb(51, 51, 51)',
+        borderColor: 'grey',
+        borderStyle: 'solid',
+        borderWidth: 0,
+        boxShadow: '0 0 0 1px grey',
+        cursor: 'default',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        outline: '0 !important',
+        position: 'relative',
+        transition: 'all 100ms',
+        paddingTop: 2
+    }),
+    option: (styles, {data, isDisabled, isFocused}) => {
+        return {
+            ...styles,
+            height: 30,
+            backgroundColor: isDisabled
+                ? null
+                : isFocused ? 'rgba(255, 255, 255, 0.1)' : null,
+        };
+    },
+
+    menuList: () => ({
+        backgroundColor: 'rgb(51, 51, 51)',
+    }),
+};
 
 /**
  * Options class passed to the react-select component
@@ -72,13 +138,12 @@ function SelectWrapped(props) {
     const {classes, ...other} = props;
     return (
         <Select
-            styles={customStyles}
+            styles={props.muiTheme.name === 'dark' ? customStyles : {}}
             optionComponent={Option}
-            noResultsText={<Typography>{'No results found'}</Typography>}
+            noResultsText={<div>{'No results found'}</div>}
             arrowRenderer={arrowProps => {
                 return arrowProps.isOpen ? <ArrowDropUpIcon/> : <ArrowDropDownIcon/>;
             }}
-            isClearable
             clearRenderer={() => <ClearIcon/>}
             valueComponent={valueProps => {
                 const {value, children, onRemove} = valueProps;
@@ -201,17 +266,15 @@ class HTTPAnalyticsResponseCodeFilter extends Widget {
         const {classes} = this.props;
         return (
             <JssProvider generateClassName={generateClassName}>
-                <MuiThemeProvider theme={customTheme}>
+                <MuiThemeProvider theme={this.props.muiTheme.name === 'dark' ? darkTheme : lightTheme}>
                     <Scrollbars style={{height: this.state.height}}>
-                        <div style={{margin: '2%', maxWidth: 840}}>
+                        <div style={{paddingLeft: 24, paddingRight: 16}}>
                             <Tabs
                                 value={this.state.perspective}
                                 onChange={(evt, value) => this.setState({perspective: value}, this.publishUpdate)}>
                                 <Tab value={3} label="Response Code"/>
                             </Tabs>
-                            <Typography component="div"
-                                        style={{'padding-top': 8, 'padding-left': 8 * 3, 'padding-right': 200}}>
-                                <TextField
+                            <TextField
                                     fullWidth
                                     value={this.state.selectedServiceValues}
                                     onChange={this.handleChange}
@@ -227,11 +290,11 @@ class HTTPAnalyticsResponseCodeFilter extends Widget {
                                             isMulti: false,
                                             simpleValue: true,
                                             options: this.state.serviceOptions,
+                                            muiTheme: this.props.muiTheme,
                                         }
                                     }}
                                 />
-                            </Typography>
-                        </div>
+                            </div>
                     </Scrollbars>
                 </MuiThemeProvider>
             </JssProvider>
